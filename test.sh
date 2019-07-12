cd4f41fe28ce113640ad01a6c46cda311d4f7e3b
grahamlyons <graham@grahamlyons.com>
2019-07-12 13:28:43 +0100

    Add `${@}` to arguments to test Docker container
    Allow other arguments to be passed to `py.test`

--- test.sh
+++ test.sh
@@ -8,4 +8,4 @@ docker image build -t "${IMAGE_ID}:test" --target=test .
 
 docker container run --rm --name "${IMAGE_ID}-test" \
     "${IMAGE_ID}:test" \
-    py.test --flake8 --cov --mypy --mypy-ignore-missing-imports
+    py.test --flake8 --cov --mypy --mypy-ignore-missing-imports "${@}"
